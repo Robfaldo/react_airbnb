b351d239d900539e95ddf80f9482b4a07087ebae
Robert Faldo <robertfaldo@gmail.com>
2018-07-22 15:50:32 +0100

    Add flat component

--- src/components/hello.js
+++ src/components/hello.js
@@ -1,6 +1,8 @@
 import React from "react";
 
+// defining the class Hello, which inherits from Component
 class Hello extends React.Component {
+  // Hello has a render method that returns html string
   render() {
     return <div>Hello {this.props.name}! </div>;
   }
