2343bf0907eef4aa52f70044b45b4586f482f5ba
Robert Faldo <robertfaldo@gmail.com>
2018-07-22 14:59:08 +0100

    Up to 32 mins into the video

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Hello from './components/hello';
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
 registerServiceWorker();
